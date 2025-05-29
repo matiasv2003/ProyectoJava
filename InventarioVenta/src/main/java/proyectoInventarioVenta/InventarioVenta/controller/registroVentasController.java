2aa70f671307dedde351b11d7d0726e5e32ec8f5
Matias Vargas <matia.vargasb@duocuc.cl>
2025-05-29 00:14:06 -0400

    Nuevo Metodo

--- InventarioVenta/src/main/java/proyectoInventarioVenta/InventarioVenta/controller/registroVentasController.java
+++ InventarioVenta/src/main/java/proyectoInventarioVenta/InventarioVenta/controller/registroVentasController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,20 @@ public class registroVentasController {
                 rvService.delete(idVenta);
                 return ResponseEntity.noContent().build();
             } catch( Exception e ) {
+                return ResponseEntity.notFound().build();
+            }
+        }
+                @PutMapping("/{idVenta}")
+        public ResponseEntity<registroVentas> actualizar(@PathVariable Integer idVenta,@RequestBody registroVentas ven) {
+            try {
+                registroVentas venre = rvService.findById(idVenta);
+                venre.setIdVenta(idVenta);
+                venre.setCantidad(ven.getCantidad());
+                venre.setPrecio(ven.getPrecio());
+                rvService.save(venre);
+                return ResponseEntity.ok(ven);
+                
+            } catch (Exception e) {
                 return ResponseEntity.notFound().build();
             }
         }
